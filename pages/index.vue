a77fbeae4de36cfbf5aa5b734ba22830f1976ee4
kango furusu <furusu0715@gmail.com>
2019-08-09 01:31:34 +0900

    create-nuxt-app

---
+++ pages/index.vue
@@ -0,0 +1,30 @@
+<template>
+  <section class="container">
+    <div>
+      <Egg />
+    </div>
+  </section>
+</template>
+
+<script>
+import Logo from '~/components/Logo.vue'
+import Egg from "../components/Egg";
+
+export default {
+  components: {
+    Egg,
+    Logo
+  }
+}
+</script>
+
+<style>
+.container {
+  margin: 0 auto;
+  min-height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+}
+</style>
